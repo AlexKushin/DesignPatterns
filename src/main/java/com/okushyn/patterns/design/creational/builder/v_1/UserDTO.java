66102716d55f2fd19b7d07533ac5a6785e336e02
kyshin <kyshin1993@gmail.com>
2025-04-09 19:02:21 +0200

    Builder - starting point of implementing builder pattern

---
+++ src/main/java/com/okushyn/patterns/design/creational/builder/v_1/UserDTO.java
@@ -0,0 +1,11 @@
+package com.okushyn.patterns.design.creational.builder.v_1;
+
+//Interface implemented by "products"
+public interface UserDTO {
+
+    String getName();
+
+    String getAddress();
+
+    String getAge();
+}
---
+++ src/main/java/com/okushyn/patterns/design/creational/builder/v_1/UserWebDTOBuilder.java
@@ -0,0 +1,7 @@
+package com.okushyn.patterns.design.creational.builder.v_1;
+
+//The concrete builder for UserWebDTO
+public class UserWebDTOBuilder {
+
+
+}

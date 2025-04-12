fb7a7071c4e50bed0c263670ca415069da7948be
kyshin <kyshin1993@gmail.com>
2025-04-12 19:50:57 +0200

    Simple Factory starting point

---
+++ src/main/java/com/okushyn/patterns/design/creational/simple_factory/Client.java
@@ -0,0 +1,10 @@
+package com.okushyn.patterns.design.creational.simple_factory;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
---
+++ src/main/java/com/okushyn/patterns/design/creational/simple_factory/PostFactory.java
@@ -0,0 +1,12 @@
+package com.okushyn.patterns.design.creational.simple_factory;
+
+
+/**
+ * This class acts as a simple factory for creation of
+ * different posts on website.
+ *
+ */
+public class PostFactory {
+
+
+}

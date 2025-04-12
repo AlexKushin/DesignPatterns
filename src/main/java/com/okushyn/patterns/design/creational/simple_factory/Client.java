071f08e43583ac26a7b8350328033e46fa0a3cb1
kyshin <kyshin1993@gmail.com>
2025-04-12 20:02:14 +0200

    Simple Factory for different types of Posts has been implemented using static method and switch statement

--- src/main/java/com/okushyn/patterns/design/creational/simple_factory/Client.java
+++ src/main/java/com/okushyn/patterns/design/creational/simple_factory/Client.java
@@ -4,7 +4,8 @@ public class Client {
 
     public static void main(String[] args) {
 
-
+        Post post = PostFactory.createPost("Blog");
+        System.out.println(post);
     }
 
 }
--- src/main/java/com/okushyn/patterns/design/creational/simple_factory/PostFactory.java
+++ src/main/java/com/okushyn/patterns/design/creational/simple_factory/PostFactory.java
@@ -4,9 +4,17 @@ package com.okushyn.patterns.design.creational.simple_factory;
 /**
  * This class acts as a simple factory for creation of
  * different posts on website.
- *
  */
 public class PostFactory {
 
+    public static Post createPost(String type) {
+        String postType = type.toLowerCase();
+        return switch (postType) {
+            case "blog" -> new BlogPost();
+            case "news" -> new NewsPost();
+            case "product" -> new ProductPost();
+            default -> throw new IllegalArgumentException("Unknown post type: " + postType);
+        };
+    }
 
 }

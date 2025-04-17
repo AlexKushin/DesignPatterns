0f6f5083c3b6ed71e154c2d30a770774ffe03bcc
kyshin <kyshin1993@gmail.com>
2025-04-17 02:05:04 +0200

    Prototype - start of implementation

---
+++ src/main/java/com/okushyn/patterns/design/creational/prototype/Client.java
@@ -0,0 +1,9 @@
+package com.okushyn.patterns.design.creational.prototype;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/okushyn/patterns/design/creational/prototype/Swordsman.java
@@ -0,0 +1,17 @@
+package com.okushyn.patterns.design.creational.prototype;
+
+
+public class Swordsman extends GameUnit {
+
+    private String state = "idle";
+
+    public void attack() {
+        this.state = "attacking";
+    }
+
+    @Override
+    public String toString() {
+        return "Swordsman " + state + " @ " + getPosition();
+    }
+
+}
